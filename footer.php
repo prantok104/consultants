f136b05b49b791b60e66e128abe590fc86ddd881
Pranto Kumar <40034555+prantok104@users.noreply.github.com>
2019-05-03 20:47:24 +0600

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,69 @@
+<section class="phy-footer-top-area-start">
+    <div class="container">
+        <div class="row">
+            <div class="phy-footer-top-content">
+                <div class="phy-footer-top-bar">
+                    <div class="col-md-6">
+                        <div class="phy-footer-left-text text-center">
+                            <p>© 2019 Physician’s Psychiatric Consultant. All rights reserved.</p>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="phy-footer-right-text text-center">
+                            <p>Developed by Nnenna Lovette Branding Agency</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<footer class="phy-footer-area-start">
+    <div class="container">
+        <div class="row">
+            <div class="phy-footer-content">
+                <div class="phy-footer-text-cell">
+                    <p>Physician’s Psychiatric Consultant</p>
+                    <a href="">Address goes here</a>
+                    <div class="social-icon">
+                        <a href=""><i class="fa fa-facebook"></i></a>
+                        <a href=""><i class="fa fa-twitter"></i></a>
+                        <a href=""><i class="fa fa-instagram"></i></a>
+                        <a href=""><i class="fa fa-youtube"></i></a>
+                    </div>
+                    <p> 1-844 PPC EVAL (772-3825)</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+<!--jQuery-3.2.1-->
+<script src="assets/js/jquery.min.js"></script>
+<!--Waypoints JS-->
+<script src="assets/js/waypoints.min.js"></script>
+<!--CounterUP JS-->
+<script src="assets/js/jquery.counterup.min.js"></script>
+<!--Bootstrap JS-->
+<script src="assets/js/bootstrap.min.js"></script>
+<!--modernizr JS-->
+<script src="assets/js/modernizr.js"></script>
+<!--Headline Animate JS-->
+<script src="assets/js/headline-animated.js"></script>
+<!--Owl-Carousel JS-->
+<script src="assets/js/owl.carousel.min.js"></script>
+<!--Pretty-Photo JS-->
+<script src="assets/js/jquery.prettyPhoto.js"></script>
+<!--Pretty-doughtnut JS-->
+<script src="assets/js/pretty-doughtnut.js"></script>
+<!--Chart JS-->
+<script src="assets/js/Chart.min.js"></script>
+<!--Paralax JS-->
+<script src="assets/js/parallax.min.js"></script>
+<!--Isotope JS-->
+<script src="assets/js/isotope.pkgd.min.js"></script>
+<!--Ripples JS-->
+<script src="assets/js/jquery.ripples-min.js"></script>
+<!--Main JS-->
+<script src="assets/js/main.js"></script>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,322 @@
+<?php include('header.php');?>
+<!--Header area start-->
+<header class="phy-header-top-content">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+                <div class="phy-header-top-left text-center">
+                    <p>Affordable, efficient, confidential and effective</p>
+                </div>
+            </div>
+            <div class="col-md-8">
+                <div class="phy-header-top-right text-center">
+                    <p>Call and leave a voicemail or text 1-844 PPC EVAL (772-3825)</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
+<!--Header area end-->
+
+<!--nav Area start-->
+<nav class="phy-nav-area-start">
+    <div class="container">
+        <div class="row">
+            <div class="phy-nav-area-content">
+                <div class="col-md-4">
+                    <div class="phy-logo-area text-center">
+                        <a href=""> <span class="circle"></span> Physician’s Psychiatric <br> Consultant</a>
+                    </div>
+                </div>
+                <div class="col-md-8">
+                    <div class="main-menu">
+                        <ul class="phy-main-menu text-center">
+                            <li><a href="#">Home</a></li>
+                            <li><a href="#">PSYCHIATRISTS</a></li>
+                            <li><a href="#">RESIDENTS & MEDICAL STUDENTS</a></li>
+                            <li><a href="#">PATIENTS & FAMILIES</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</nav>
+<!--nav Area end-->
+
+<!--Slider area start-->
+<section class="phy-banner-area-start">
+    <div class="container">
+        <div class="row">
+            <div class="phy-banner-content">
+                <div class="col-md-5">
+                    <div class="phy-banner-text">
+                        <p>We Can Help You <br> <span class="text-bold">Get Better</span> <span class="text-large">&</span></p>
+                        <button class="phy-style-btn">Regain Your Smile</button>
+                        <p class="phy-left-margin">Lorem ipsum dolor sit amet, id his commodo indoctum reformidans, ei sit quis saperet appellantur, cum saepe iriure suscipit ne. Movet persequeris sed an, veniam feugiat no vis. Mea an impedit elaboraret. Id posse iriure inimicus pri.
+                        </p>
+                        <a href="" class="btn">Learn more</a>
+                    </div>
+                </div>
+                <div class="phy-banner-form">
+                    <div class="phy-banner-form-content">
+                        <h2>NEED HELP?</h2>
+                        <p>USE THE FORM BELOW TO ASK A QUESTION AND A DOCTOR WILL BE RIGHT WITH YOU</p>
+                        <form action="" method="POST">
+                            <input type="text" name="" id="" placeholder="First Name">
+                            <input type="text" name="" id="" placeholder="Last Name">
+                            <input type="text" name="" id="" placeholder="City">
+                            <textarea name="" id="" cols="50" rows="5" placeholder="Question"></textarea>
+                            <button class="phy-form-btn">Get answer</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!--Slider area end-->
+
+<!--About area start-->
+<section class="phy-about-area-start">
+    <div class="container">
+        <div class="row">
+            <div class="phy-about-content">
+                <h1>About <span class="text-color">Us</span></h1>
+                <p>Physician’s Psychiatric Consultation (PPC) provides a much needed opportunity for physicians, including resident/fellow trainees and medical students, to obtain quality pschiatric care that is affordable, efficient, confidential and effective. Our goal at PPC is to eliminate barriers that prevent our popultation of highly intelligent physicians and aspiring doctors from obtaining psychiatric and other medical care.</p>
+                <a href="" class="phy-btn">Read more</a>
+            </div>
+        </div>
+    </div> <!-- Phy About area end -->
+    <div class="phy-feature-area-start">
+    <div class="container">
+        <div class="row">
+            <div class="phy-geature-content">
+                <div class="col-md-4">
+                    <div class="phy-single-feature">
+                        <div class="phy-fea-icon">
+                            <i class="fa fa-check-circle-o"></i>
+                        </div>
+                        <div class="phy-fea-content">
+                            <h2>FEATURES AND BENEFITS</h2>
+                            <p>Eam civibus adipisci scriptorem et, cum ad case deserunt consequat. Veri democritum mei ea. Nam ex ferri possit praesent, dolores expetenda corrumpit has te.</p>
+                            <a href="">CLICK HERE</a>
+                        </div>
+                    </div>
+                </div> <!-- Single feature end -->
+                <div class="col-md-4">
+                    <div class="phy-single-feature">
+                        <div class="phy-fea-icon">
+                            <i class="fa fa-check-circle-o"></i>
+                        </div>
+                        <div class="phy-fea-content">
+                            <h2>FEATURES AND BENEFITS</h2>
+                            <p>Eam civibus adipisci scriptorem et, cum ad case deserunt consequat. Veri democritum mei ea. Nam ex ferri possit praesent, dolores expetenda corrumpit has te.</p>
+                            <a href="">CLICK HERE</a>
+                        </div>
+                    </div>
+                </div> <!-- Single feature end -->
+                <div class="col-md-4">
+                    <div class="phy-single-feature">
+                        <div class="phy-fea-icon">
+                            <i class="fa fa-check-circle-o"></i>
+                        </div>
+                        <div class="phy-fea-content">
+                            <h2>FEATURES AND BENEFITS</h2>
+                            <p>Eam civibus adipisci scriptorem et, cum ad case deserunt consequat. Veri democritum mei ea. Nam ex ferri possit praesent, dolores expetenda corrumpit has te.</p>
+                            <a href="">CLICK HERE</a>
+                        </div>
+                    </div>
+                </div> <!-- Single feature end -->
+            </div>
+        </div>
+    </div>
+    </div>
+</section>
+<!--About area end-->
+
+<!--Appointment area start-->
+<section class="phy-appointment-area-start">
+    <div class="container">
+        <div class="row">
+            <div class="phy-appointment-content">
+                <div class="col-md-6">
+                    <div class="phy-appointment-text">
+                        <p>Providing comprehensive, confidential psychiatric consultation and other medical care exclusively for physicians.</p>
+                        <p>Affordable self-pay options to minimize insurance burden and third party intrusions.</p>
+                        <p>Media-based Telemedicine appointments to maximize accessibility.</p>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="phy-appointment-form">
+                        <div class="phy-banner-form-content">
+                            <h2>GET AN APPOINTMENT</h2>
+                            <p>FILL-UP THE FORM BELOW AND WE WILL PROCESS YOUR APPOINTMENT</p>
+                            <form action="" method="POST">
+                                <input type="text" name="" id="" placeholder="Full Name">
+                                <input type="text" name="" id="" placeholder="Phone">
+                                <button class="phy-form-btn">BOOK AN APPOINTMENT</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!--Appointment area end-->
+
+<!--Testimonial area start-->
+<section class="phy-testimonial-area-start">
+    <div class="container">
+        <div class="row">
+            <div class="phy-testimonial-content">
+                <div class="col-md-6">
+                    <div class="phy-work-content">
+                        <h1>HOW IT <div class="text-color">WORK</div></h1>
+                        <div class="single-service">
+                            <p>Call or text your name and contact information, your preferred method of contact and your availability for your free pre-appointment.
+                            </p>
+                            <p>If you both determine that a comprehensive medical evaluation with a focus on psychiatric symptoms is warranted, self-pay options will be agreed upon and an appointment will be scheduled at a time and day you  are available.</p>
+                            <p>You will also receive information on how to effectively use the telemedicine platform(s) from a private setting of your choosing that is conducive for your appointment(s).
+                            </p>
+                            <p>Your initial evaluation will include a 1-2 hour telemedicine session which will optimized the likelyhood of obtaining an appointment as soon as your schedule permits and effectively eliminate the added stress of transportation, work/class scheduling conflicts and other time-wasters interfering with scheduling and keeping your appointment.</p>
+                            <p>The focus of the first session will include a comprehensive diagnostic assessment, workup plan and acute problem solving to address your pressing medical concerns and the psychosocial factors affecting them. </p>
+                            <p>Together with your psychiatric consultant, you will determine 1) future steps to achieve/maintain and sustain stability, 2) short and long term goals of treatment/consultation, 3) whether additional sessions are needed and 4) whether/how to transition care to or incorporate a trusted primary care provider if appropriate to ensure best medical practices and optimize affordability.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="phy-testimonial-area">
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!--Testimonial area end-->
+
+<!--Services area start-->
+<section class="phy-services-area-start">
+    <div class="container">
+        <div class="row">
+            <div class="phy-services-content">
+                <div class="col-md-5">
+                    <div class="phy-services-img">
+                        <img src="assets/img/services.jpg" alt="">
+                    </div>
+                </div>
+                <div class="col-md-7">
+                    <div class="phy-services-context">
+                        <h2>OUR <span class="text-color">SERVICES</span></h2>
+                        <div class="row">
+                            <div class="col-md-6">
+                                <div class="single-service">
+                                    <p>Provides quality psychiatric care that is affordable, efficient, confidential and effective.</p>
+                                </div>
+                            </div> <!-- Single services end -->
+                            <div class="col-md-6">
+                                <div class="single-service">
+                                    <p>Eliminate barriers that prevent physicians and aspiring doctors  from obtaining psychiatric and other medical care.</p>
+                                </div>
+                            </div> <!-- Single services end -->
+                            <div class="col-md-6">
+                                <div class="single-service">
+                                    <p>We aim to optimize healthcare provision to physicians by offering a comprehensive, physician-centered approach.</p>
+                                </div>
+                            </div> <!-- Single services end -->
+                            <div class="col-md-6">
+                                <div class="single-service">
+                                    <p>We provide the care you need, the way that works best for you!</p>
+                                </div>
+                            </div> <!-- Single services end -->
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!--Services area end-->
+
+<!--Broc aera start-->
+<section class="phy-broc-area-start">
+    <div class="container">
+        <div class="row">
+            <div class="phy-broc-area-content">
+                <div class="col-md-6">
+                    <div class="phy-broc-content">
+                        <h2>DID YOU <span class="text-color">KNOW?</span></h2>
+                        <p>Physicians have the highest suicide rate of any profession! An average of one Physician commits suicide in the U.S. everyday. Physician suicide rates are more thatn twice that of the general population.</p>
+                        <div class="text-center">
+                            <a href="">DOWNLOAD BROCHURE</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="phy-broc-img">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!--Broc aera end-->
+
+<!--Contact area start-->
+<section class="phy-contact-area-start">
+    <div class="container">
+        <div class="row">
+            <div class="phy-contact-area-content">
+                <div class="phy-contact-left">
+                    <div class="phy-contact-form">
+                        <div class="phy-banner-form-content">
+                            <h2>ABOUT OUR PSYCHIATRIC <br> <span class="text-bold">CONSULTANT</span></h2>
+                            <ul class="contact-list">
+                                <li>Ngozi Nkanginieme, MD</li>
+                                <li>Consultation - Liaison Psychiatrist </li>
+                                <li>Residency trained in Psychiatry at the Cleveland Clinic Hospital </li>
+                                <li>Fellowship trained in Psychosomatic Medicine / Consultation - </li>
+                                <li>Liaison Psychiatry at The Massachusetts General Hospital</li>
+                            </ul>
+                            <h3>SUBMIT A QUESTION TO OUR DOCTOR USING THE FORM BELOW</h3>
+                            <form action="" method="POST">
+                                <input type="text" name="" id="" placeholder="Full Name">
+                                <input type="email" name="" id="" placeholder="Email">
+                                <textarea name="" id="" cols="30" rows="4" placeholder="Question"></textarea>
+                                <button class="phy-form-btn">SUBMIT QUESTION</button>
+                            </form>
+                        </div>
+                    </div>
+                </div> <!-- Single Contact end -->
+                <div class="phy-contact-right">
+                    <div class="phy-img" ></div>
+                    <div class="phy-single-context color-fade">
+                        <div class="phy-single-context-cell">
+                            <h2>OFFICE HOURS</h2>
+                            <p>Mon - Fri 8:00AM - 7:00PM</p>
+                            <p>Saturday 8:00AM - 5:00PM</p>
+                        </div>
+                    </div>
+                    <div class="phy-single-context color-medium">
+                        <div class="phy-single-context-cell">
+                            <h2>CONTACT US</h2>
+                            <p>Call and leave a voicemail or text</p>
+                            <p>1-844 PPC EVAL (772-3825)</p>
+                        </div>
+                    </div>
+                    <div class="phy-single-context color-deep">
+                        <div class="phy-single-context-cell">
+                            <h2>WHERE WE ARE</h2>
+                            <p>Address goes here</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!--Contact area end-->
+
+<?php include('footer.php');?>
